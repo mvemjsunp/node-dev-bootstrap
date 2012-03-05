5b53c42d59b0f3c97743c0d60e23d7f53985f07a
Rebekah David <rldavid@unca.edu>
2012-03-05 16:13:59 -0500

    added functionality to count matches via redis

--- twitter.js
+++ twitter.js
@@ -1,19 +1,55 @@
 var twitter = require('ntwitter');
+var redis = require('redis');
 var credentials = require('./credentials.js');
 
-var twitter = new twitter({
+//create redis client
+var client = redis.createClient();
+
+//if the 'awesome' key doesn't exist, create it
+//redis gives a value to error and exists
+client.exists('awesome', function(error, exists) {
+	//if error is define, then there was probably some 
+	//problem connecting to redis
+	if(error) {
+		console.log('ERROR: '+error);
+	} 
+	//otherwise exists will be available, and we can do something with it
+	else if(!exists) {
+		client.set('awesome', 0); //create the awesome key
+	};
+});
+
+var t = new twitter({
 	consumer_key: credentials.consumer_key,
 	consumer_secret: credentials.consumer_secret,
 	access_token_key: credentials.access_token_key,
 	access_token_secret: credentials.access_token_secret
 });
 
-twitter.stream(
+t.stream(
 	'statuses/filter',
 	{ track: ['awesome', 'cool', 'rad', 'gnarly', 'groovy'] },
 	function(stream) {
 		stream.on('data', function(tweet) {
 			console.log(tweet.text);
+			//if awesome is in the tweet text, increment counter
+			if(tweet.text.match(/awesome/)) {
+				client.incr('awesome');
+			}
+			else if(tweet.text.match(/cool/)) {
+				client.incr('cool');
+			}
+			else if(tweet.text.match(/rad/)) {
+				client.incr('rad');
+			}
+			else if(tweet.text.match(/gnarly/)) {
+				client.incr('gnarly');
+			}
+			else if(tweet.text.match(/groovy/)) {
+				client.incr('groovy');
+			}
+			else {
+			}
 		});
 	}
 );
