d2a5fa3d9025c9a129e35b02aa388505badbde95
Rebekah David <rldavid@unca.edu>
2012-03-14 10:03:46 -0400

    added other search terms to display in browser with counts

--- server.js
+++ server.js
@@ -2,12 +2,37 @@ var http = require('http');
 var redis = require('redis');
 
 var client = redis.createClient();
-var awesomeCount = client.get('awesome');
+//var awesomeCount = client.get('awesome');    
 
+// http.createServer(function (req, res) {
+// 	client.get('awesome',function(error, awesomeCount) {	//send in callback, so when it
+// 		client.get('cool',function(error, coolCount) {		//responds function is called
+// 			client.get('rad',function(error, radCount) {
+// 				client.get('gnarly',function(error, gnarlyCount) {
+// 					client.get('groovy',function(error, groovyCount) {
+// 			
+// 		if(error) {											
+// 			awesomeCount = error;	//if not really necessary, presented here for clarity
+// 		}
+// 		res.writeHead(200, {'Content-Type': 'text/html'});
+// 	    res.end('awesome count: ' + awesomeCount + '<br /> cool count : ' + coolCount + 
+// 		'<br /> rad count : ' + radCount + '<br /> gnarly count : ' + gnarlyCount + 
+// 		'<br /> groovy count : ' + groovyCount);
+// 					});
+// 				});
+// 			});
+// 		});
+// 	});
+	
 http.createServer(function (req, res) {
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('awesome count: ' + awesomeCount);
+	client.mget(['awesome','gnarly'], function(error, responses) {
+		console.log(responses);
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('awesomeCount:' + responses[0] + '<br /> gnarlyCount:' + responses[1]);
+	});
 }).listen(3000);
+
+
+// }).listen(3000);
 
 console.log('Server running on port 3000');
--- twitter.js
+++ twitter.js
@@ -3,21 +3,21 @@ var redis = require('redis');
 var credentials = require('./credentials.js');
 
 //create redis client
-var client = redis.createClient();
+var client = redis.createClient();	//connects to locally-running REDIS in vagrant instance
 
 //if the 'awesome' key doesn't exist, create it
 //redis gives a value to error and exists (boolean)
-client.exists('awesome', function(error, exists) {
+/*client.exists('awesome', function(error, exists) {  //when redis database responds, calls this function that takes two arguments -- error and exists
 	//if error is define, then there was probably some 
 	//problem connecting to redis
-	if(error) {
+	if(error) {			
 		console.log('ERROR: '+error);
 	} 
 	//otherwise exists will be available, and we can do something with it
 	else if(!exists) {
 		client.set('awesome', 0); //create the awesome key
 	};
-});
+});*/			//this code isn't necessary since incrementing it creates it if it doesn't already exists
 
 var t = new twitter({
 	consumer_key: credentials.consumer_key,
@@ -35,6 +35,14 @@ t.stream(
 			//if awesome is in the tweet text, increment counter
 			if(tweet.text.match(/awesome/)) {
 				client.incr('awesome');
+			} else if(tweet.text.match(/cool/)) {
+				client.incr('cool');
+			} else if(tweet.text.match(/rad/)) {
+				client.incr('rad');
+			} else if(tweet.text.match(/gnarly/)) {
+				client.incr('gnarly');
+			} else if(tweet.text.match(/groovy/)) {
+				client.incr('groovy');
 			}
 		});
 	}
